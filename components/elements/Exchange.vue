c5d12e35b93561ef1a8a3b0d3d46c304433bdf65
Bexruz <74446521+dumsterB@users.noreply.github.com>
2022-02-16 16:51:41 +0500

    exchange fileds

--- components/elements/Exchange.vue
+++ components/elements/Exchange.vue
@@ -12,32 +12,33 @@
             class="mr-2"
             solo
           ></v-text-field>
-          <v-select
-            :items="items"
-            label="BNB"
-            class="ml-2"
-            solo
-          >
-            <template v-slot:item="{ item }">
-              <img height="20" width="20" :src="item.img"> <p class="ml-4 mt-3">{{ item.currency }}</p>
-            </template>
-          </v-select>
+        <v-select
+          :items="currencies"
+          label="BNB"
+          class="ml-2"
+          item-text="name"
+          solo
+        >
+          <template v-slot:item="{ item }">
+            <img height="20" width="20" :src="item.logo"> <p class="ml-4 mt-3">{{ item.name }}</p>
+          </template>
+        </v-select>
       </div>
       <div class="pa-1 mt-2 justify-space-between d-flex"  >
         <v-text-field
-          label="Оплатить в 000 "
-          disabled
+          label="Оплатить в"
           class="mr-2"
           solo
         ></v-text-field>
         <v-select
-          :items="items"
+          :items="currencies"
           label="BNB"
           class="ml-2"
+          item-text="name"
           solo
         >
           <template v-slot:item="{ item }">
-            <img height="20" width="20" :src="item.img"> <p class="ml-4 mt-3">{{ item.currency }}</p>
+            <img height="20" width="20" :src="item.logo"> <p class="ml-4 mt-3">{{ item.name }}</p>
           </template>
         </v-select>
       </div>
@@ -48,12 +49,28 @@
 </template>
 
 <script>
+import {mapActions, mapGetters} from "vuex";
+const currencies = 'data/currency'
 export default {
   name: "Exchange",
   data(){
     return{
-      items: [{currency:'Foo',img:'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png'},{ currency:'Bar',img:'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png'},{currency:'Fizz',img:'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png'}, {currency:'Buzz',img:'https://s2.coinmarketcap.com/static/img/coins/64x64/2010.png'}],
+      items: [
+        {currency:'Foo',img:'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png'},
+        { currency:'Bar',img:'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png'},
+        {currency:'Fizz',img:'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png'},
+        {currency:'Buzz',img:'https://s2.coinmarketcap.com/static/img/coins/64x64/2010.png'}],
     }
+  },
+  methods:{
+    ...mapActions(currencies, {
+      fetchCurrencies: "fetchList",
+    }),
+  },
+  computed:{
+    ...mapGetters("data/currency", {
+      currencies: "list",
+    }),
   }
 }
 </script>
