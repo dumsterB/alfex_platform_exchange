1f8de31c6869e7540a797aa56a9c71d577c94ffa
Bexruz <74446521+dumsterB@users.noreply.github.com>
2022-02-16 23:58:36 +0500

    wallet components

---
+++ components/elements/Check.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <v-card
+      class="mx-auto pa-2 ml-2 check-card"
+      max-width="400"
+      outlined
+    >
+      <v-list-item three-line>
+        <v-list-item-content>
+          <v-list-item-title class="text-h5 mb-1">
+            <strong>{{title}}</strong>
+          </v-list-item-title>
+          <br><br>
+          <v-row>
+            <v-col>
+              <p class="text-gray">{{$t('total_equity')}}</p>
+              <p class="text-h6"> <strong>{{totalEquity}}</strong></p>
+              <p class="text-gray">{{total_equity_usdt}}</p>
+            </v-col>
+            <v-col>
+              <p class="text-gray">{{$t('available_balance')}}</p>
+             <p class="text-h6"><strong>{{available_balance}}</strong></p>
+              <p class="text-gray">{{available_balance_usdt}}</p>
+            </v-col>
+          </v-row>
+        </v-list-item-content>
+
+
+      </v-list-item>
+    </v-card>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props:['title','totalEquity','available_balance','available_balance_usdt','available_balance_usdt','total_equity_usdt'],
+  name: "GeneralCapital"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/elements/GeneralCapital.vue
@@ -0,0 +1,55 @@
+<template>
+<div>
+  <v-card
+    class="mx-auto pa-2"
+    max-width="350"
+    outlined
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title class="text-h5 mb-1">
+         <strong>{{$t('total_equity')}}</strong>
+        </v-list-item-title>
+        <br><br>
+        <v-list-item-title class="text-h5 mb-1">
+       <strong>93.173529 </strong>BTC
+        </v-list-item-title>
+        <p class="mt-2 text-gray">≈ 4,064,351.997 USD</p>
+      </v-list-item-content>
+
+    <v-icon>mdi-eye</v-icon>
+    </v-list-item>
+
+    <v-card-actions class="justify-center">
+      <v-btn
+        elevation="0"
+        large
+        rounded
+        class="success-btn-half"
+        color="white"
+      >
+        {{$t('deposit_title')}}
+      </v-btn>
+      <v-btn
+        elevation="0"
+        large
+        rounded
+        class="simple-btn-half"
+      >
+        {{$t('withdraw')}}
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "GeneralCapital"
+}
+</script>
+
+<style scoped>
+
+</style>
