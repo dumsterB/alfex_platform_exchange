c280a5e7bd71b8d8345be01ad8f69d8379640e8e
Tair <abduraimovtair@gmail.com>
2022-02-16 11:56:26 +0200

    add trade graph

--- pages/currency/index.vue
+++ pages/currency/index.vue
@@ -25,11 +25,16 @@
           </v-col>
         </v-row>
       </v-col>
+      <v-col :cols="12" :md="9" :lg="9" :sm="12" :xs="12">
+        <Indicators></Indicators>
+      </v-col>
     </v-row>
     <v-row>
       <v-col :cols="12" :md="8" :lg="8" :sm="12" :xs="12">
         <v-row class="ml-4">
-          <v-col class="d-flex justify-center"> </v-col>
+          <v-col class="d-flex justify-center">
+            <TradeGraph :width="graphWidth" :height="graphHeight"></TradeGraph>
+          </v-col>
         </v-row>
       </v-col>
       <v-col :cols="12" :md="4" :lg="4" :sm="12" :xs="12"> </v-col>
@@ -39,14 +44,21 @@
 
 <script>
 import { mapGetters, mapActions } from "vuex";
+import Indicators from "~/components/elements/currencies/Indicators";
+import TradeGraph from "~/components/graphs/Trade";
 const model = "data/currency";
 
 export default {
-  components: {},
+  components: {
+    Indicators,
+    TradeGraph,
+  },
   data() {
     return {
       is_switched: false,
       curr_id: null,
+      graphWidth: window.innerWidth * 2 / 3 - 300,
+      graphHeight: 600,
     };
   },
   computed: {
@@ -72,11 +84,18 @@ export default {
     ...mapActions("data/arbitrage_company", {
       fetchAC: "fetchList",
     }),
+    onResize(event) {
+      this.graphWidth = window.innerWidth * 2 / 3 - 400;
+    },
   },
   created() {},
   mounted() {
     this.fetchCurrencies();
     this.fetchAC();
+    window.addEventListener("resize", this.onResize);
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.onResize);
   },
 };
 </script>
