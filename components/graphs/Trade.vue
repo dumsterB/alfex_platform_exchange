a619b011934faa8a4635b40583c96ed9dd1a76a1
Tair <abduraimovtair@gmail.com>
2022-02-18 10:55:03 +0200

    add trandingview graph

--- components/graphs/Trade.vue
+++ components/graphs/Trade.vue
@@ -1,20 +1,26 @@
 <template>
-  <trading-vue
-    :data="chart"
-    :width="width"
-    :height="height"
-    :title-txt="title"
-    :toolbar="true"
-    :overlays="overlays"
-    :color-back="colors.back"
-    :color-grid="colors.grid"
-    :color-text="colors.text"
-    :color-cross="colors.cross"
-    :color-candle-dw="colors.candle_dw"
-    :color-wick-dw="colors.wick_dw"
-    :color-title="colors.tvTitle"
-    ref="tvjschart"
-  ></trading-vue>
+  <!-- <trading-vue
+      :data="chart"
+      :width="width"
+      :height="height"
+      :title-txt="title"
+      :toolbar="true"
+      :overlays="overlays"
+      :color-back="colors.back"
+      :color-grid="colors.grid"
+      :color-text="colors.text"
+      :color-cross="colors.cross"
+      :color-candle-dw="colors.candle_dw"
+      :color-wick-dw="colors.wick_dw"
+      :color-title="colors.tvTitle"
+      ref="tvjschart"
+    ></trading-vue> -->
+  <!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container" id="root-tradingview">
+  <div id="tradingview_22087"></div>
+  <div class="tradingview-widget-copyright"></div>
+</div>
+<!-- TradingView Widget END -->
 </template>
 <script>
 import { DataCube } from "trading-vue-js";
@@ -79,5 +85,31 @@ export default {
       this.$refs.tvjschart.resetChart();
     },
   },
+  mounted() {
+    let dcr = document.createElement("script");
+    dcr.setAttribute("type", "text/javascript");
+    dcr.setAttribute("src", "https://s3.tradingview.com/tv.js");
+    document.getElementById("root-tradingview").appendChild(dcr);
+    let crtobj = document.createElement("script");
+    crtobj.setAttribute("type", "text/javascript");
+    crtobj.innerHTML = `new TradingView.widget(
+    {
+      "width": 980,
+      "height": 610,
+      "symbol": "BYBIT:BTCUSD",
+      "interval": "D",
+      "timezone": "Etc/UTC",
+      "theme": "light",
+      "style": "1",
+      "locale": "ru",
+      "toolbar_bg": "#f1f3f6",
+      "enable_publishing": false,
+      "hide_side_toolbar": false,
+      "allow_symbol_change": true,
+      "container_id": "tradingview_22087"
+    }
+    );`;
+    document.getElementById("root-tradingview").appendChild(crtobj);
+  }
 };
 </script>
