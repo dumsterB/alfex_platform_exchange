349482c0a543e6640efd349bdbc8c80209f09533
Tair <abduraimovtair@gmail.com>
2022-03-07 20:58:08 +0200

    fix bug in tradePosition modal

--- components/elements/modals/TradePosition.vue
+++ components/elements/modals/TradePosition.vue
@@ -14,11 +14,11 @@
     </v-card-title>
     <v-card-text>
       <v-container class="d-flex justify-lg-space-between font-weight-medium">
-        <span class="dotsLine">{{ $t("marketplace_price") }}</span>
-        <span>${{ price }}</span>
+        <span>{{ $t("marketplace_price") }}</span>
+        <span>{{ price }} USD</span>
       </v-container>
       <v-container class="d-flex justify-lg-space-between">
-        <span class="dotsLine">{{ `${$t("available_balance")}` }}</span>
+        <span>{{ `${$t("available_balance")}` }}</span>
         <span>{{ balance }}</span>
       </v-container>
       <v-container>
@@ -53,14 +53,14 @@ export default {
   name: "TradePosition",
   data() {
     return {
-      amount:'',
-      wl:{},
+      amount: "",
+      wl: {},
       rules: [
         // $t("amount_required")
         (value) => !!value || this.$t("amount_required"),
         (value) => (value && value.length >= 1) || "Min 1 characters",
       ],
-      loading: false
+      loading: false,
     };
   },
   props: {
@@ -97,13 +97,17 @@ export default {
     ...mapGetters("data/wallet", {
       wallet: "list",
     }),
-    balance(){
-      if(this.action ==='Sell'){
-        return this.userWallet.balance + ' ' +this.userWallet.currency.symbol
-      }else {
-        return  this.wl.balance + ' ' + this.wl.currency.symbol
+    balance() {
+      if (this.userWallet.currency && this.wl.currency) {
+        if (this.action === "Sell") {
+          return (
+            this.userWallet.balance + " " + this.userWallet.currency.symbol
+          );
+        } else {
+          return this.wl.balance + " " + this.wl.currency.symbol;
+        }
       }
-    }
+    },
   },
   methods: {
     ...mapActions("data/arbitrage_session", {
@@ -116,32 +120,33 @@ export default {
     async save() {
       this.loading = true;
       let as_data = {
-        amount:this.amount,
-        arbitrage_company_id:this.tradePlatform.id,
-        session_start_type_id: this.action === 'Buy' ? 1 : 2,
-        exchange_wallet_id: this.action ==='Buy' ? this.wl.id : this.userWallet.id,
-        wallet_id: this.action === 'Buy' ? this.userWallet.id : this.wl.id,
+        amount: this.amount,
+        arbitrage_company_id: this.tradePlatform.id,
+        session_start_type_id: this.action === "Buy" ? 1 : 2,
+        exchange_wallet_id:
+          this.action === "Buy" ? this.wl.id : this.userWallet.id,
+        wallet_id: this.action === "Buy" ? this.userWallet.id : this.wl.id,
       };
       // code
       console.log("as_data", as_data, this.wl);
       as_data.start_exchange_rate = this.price;
-       let rs = await this.as_create({data: as_data});
-       console.log("rs", rs);
-        this.fetchWallet()
+      let rs = await this.as_create({ data: as_data });
+      console.log("rs", rs);
+      this.fetchWallet();
       setTimeout(() => {
         this.loading = false;
       }, 500);
     },
     close() {
-      this.$emit('close');
-    }
+      this.$emit("close");
+    },
   },
   async created() {
     await this.fetchAs();
     console.log("this.as", this.as);
-    console.log( this.userWallet, 'wallet')
-    this.wl=this.wallet.find(el => el.currency.symbol == "USD") || {};
-  }
+    console.log(this.userWallet, "wallet");
+    this.wl = this.wallet.find((el) => el.currency.symbol == "USD") || {};
+  },
 };
 </script>
 
