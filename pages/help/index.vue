99de78e11a671376bbcb2b7e3f44efd37814ca20
Bexruz <74446521+dumsterB@users.noreply.github.com>
2022-03-11 23:07:17 +0500

    question page finished

---
+++ pages/help/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-cantainer class="pa-2">
+      <v-row>
+         <v-col>
+         </v-col>
+      </v-row>
+    </v-cantainer>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
