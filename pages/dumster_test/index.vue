95b0102f9f668be424ac53cdecc809bdd6dc7a30
Bexruz <74446521+dumsterB@users.noreply.github.com>
2022-03-25 01:08:23 +0500

    learn to work with socket

--- pages/dumster_test/index.vue
+++ pages/dumster_test/index.vue
@@ -2,26 +2,20 @@
   <div>
     <v-container>
       <v-row class="ml-15">
-        <table>
+        <table >
           <tr>
-            <th>Company</th>
-            <th>Contact</th>
-            <th>Country</th>
+            <th>Base</th>
+            <th>Price</th>
+            <th>Date</th>
           </tr>
-          <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>Germany</td>
-          </tr>
-          <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
+          <tr  v-for="(curr,i) of currencies" :key="i">
+            <td>{{curr.base}}</td>
+            <td>{{curr.price}}</td>
+            <td>{{curr.time}}</td>
           </tr>
         </table>
       </v-row>
     </v-container>
-    {{currencies}}
   </div>
 </template>
 
@@ -40,13 +34,13 @@ export default {
       "method":"subscribe",
       "data":["gate_all@ticker_10s"]
     }`)
-      socket.onmessage=function (event){
+      socket.onmessage =(event)=>{
       if(event.data){
         let json_data=JSON.parse(event.data)
         console.log(json_data.data.data)
         this.currencies=json_data.data.data
       }
-    } 
+    }
   },
   destroyed() {
     let socket = global.socket;
