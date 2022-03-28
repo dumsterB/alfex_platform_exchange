494124dccc5226b54e26322a199953acf715fdec
Bexruz <74446521+dumsterB@users.noreply.github.com>
2022-03-28 16:14:26 +0500

    deploy project

---
+++ pages/dumster/index.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "index.vue",
+  data(){
+    return{
+      currencies:[]
+    }
+  },
+ async mounted(){
+    let socket=global.socket
+    await socket.send(`{
+
+         }`)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
